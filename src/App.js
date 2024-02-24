b8ecb8890065b0a39ada7dbd9bf07e6afe27cf49
rgarner6352 <rpg0004@mix.wvu.edu>
2024-02-24 15:12:34 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>CS 230L</h1>
+        <h2>Section - 001</h2>
+        <p>WVU ID: 800253311</p>
+        <p>Hi I am Ryan Garner</p>
+    </div>
+  );
+}
+
+export default App;
