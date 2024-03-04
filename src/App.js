d8780359523b5eb103a087cda7f44c32c4144d24
rgarner6352 <rpg0004@mix.wvu.edu>
2024-03-03 19:04:44 -0500

    Add components to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
+      <div className="App">
+          <Navbar />
         <h1>CS 230L</h1>
         <h2>Section - 001</h2>
         <p>WVU ID: 800253311</p>
-        <p>Hi I am Ryan Garner</p>
+          <p>Hi I am Ryan Garner</p>
+          <Card />
+          <Card />
+          <Card />
     </div>
   );
 }
