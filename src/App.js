9d11c9fa78847e215a3b74c3c087f54049b3284e
rgarner6352 <rpg0004@mix.wvu.edu>
2024-03-03 19:31:47 -0500

    Format Cards

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ function App() {
         <h1>CS 230L</h1>
         <h2>Section - 001</h2>
         <p>WVU ID: 800253311</p>
-          <p>Hi I am Ryan Garner</p>
-          <Card />
-          <Card />
-          <Card />
+          <div className="card-container">
+              <Card />
+              <Card />
+              <Card />
+          </div>
     </div>
   );
 }
